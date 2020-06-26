b240a9210a32aa09c9a1e593ef2f63cf2f04b1f4
Deni <deni.khalikov@gmail.com>
2020-06-26 18:27:08 +0200

    added cleaner code

--- rps.js
+++ rps.js
@@ -20,38 +20,40 @@ else
     computerChoice = "scissors"
 }
 
-console.log("Computer chose" + computerChoice);
+console.log("Computer chose " + computerChoice);
 
-var compare = function(choice1, choice2)
+var compare = function(playerChoice, computerChoice)
 {
-    if(choice1 == choice2)
-    {
-        console.log("it's a tie!");
-    }
-    else if(choice1 === "rock")
-    {
-        if(choice2 === "scissors")
-        {
-            console.log("rock wins");
-        }
-        else
-        {
-            console.log("paper wins");
-        }
-    }
-    else if(choice2 === "paper")
-    {
-        if(choice2 === "rock")
-        {
-            console.log("paper wins!");
-        }
-        else
-        {
-            console.log("scissors win");
-        }
-    }
-
-
+  if(playerChoice === computerChoice)
+  {
+      console.log("It's a tie!")
+  }
+  else if (playerChoice === "rock" && computerChoice === "scissors")
+  {
+      console.log("You win!");
+  }
+  else if (playerChoice === "rock" && computerChoice === "paper")
+  {
+      console.log("You lose!");
+  }
+  else if (playerChoice === "paper" && computerChoice === "rock")
+  {
+      console.log("You win!");
+  }
+  else if (playerChoice === "paper" && computerChoice === "scissors")
+  {
+      console.log("You lose!");
+  }
+  else if (playerChoice === "scissors" && computerChoice === "rock")
+  {
+      console.log("You lose!");
+  }
+  else if (playerChoice === "scissors" && computerChoice === "paper")
+  {
+      console.log("You win!");
+  }
 }
 
+// calling the compare function
 compare(playerChoice, computerChoice);
+console.log("player chose:", playerChoice, "& computer chose:", computerChoice);
